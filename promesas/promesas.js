aece3ac521c5c57104325ca8071a4cf02ea853bb
AgBasilio <agustinbasilio@gmail.com>
2023-02-10 22:43:49 +0100

    fs/promises como implementacion de promisify

--- promesas/promesas.js
+++ promesas/promesas.js
@@ -1,16 +1,21 @@
+/* de esta manera utilizamos promisify
 const { readFile } = require("fs");
 const { promisify } = require("util");
 
 //genera la estructura de promesas para readFile, readFile ya es async
-const readFilePromise = promisify(readFile);
+const readFilePromise = promisify(readFile); */
+
+//otra forma de hacerlo donde la promesa ya esta implementada
+const { readFile } = require("fs/promises");
 
 async function readFileAsync(fileName) {
   try {
-    const text = await readFilePromise(fileName, "utf-8");
+    // const text = await readFilePromise(fileName, "utf-8");
+    const text = await readFile(fileName, "utf-8");
     console.log({ text });
   } catch (error) {
     console.error(error);
   }
 }
 
-readFileAsync(__dirname+"/textfile.txt");
+readFileAsync(__dirname + "/textfile.txt");
